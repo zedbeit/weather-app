63b33b74aba8d280c8615ae539af75ca809633a6
deehaz <your.beitazaheed5@gmail.com>
2019-12-31 01:29:29 -0800

    add error handling for geocoding API

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const request = require('request')
 const url = 'https://api.darksky.net/forecast/d97b2f7c517de91eca994ef49a5402cc/37.8267,-122.4233'
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemVkMTIzIiwiYSI6ImNrNHNoempkZzB6Zm4za3A1dGo5emcyeXkifQ.Ed3jZfEjQvr_rLKqwBG91w'
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemVkMTIzIiwiYSI6ImNrNHNoempkZzB6Zm4za3A1dGo5emcyeXkifQ.Ed3jZfEjQvr_rLKqwBG91w&limit=1'
 
+// Geocoding Request
 request({ url: geocodeURL, json: true }, ( error, response) => {
 	const dataFeatures = response.body.features
+	if(error){
+		console.log('Unable to connect to the Weather service, please check your internet connection')
+	} else if(dataFeatures.length === 0) {
+		console.log('Invalid search request')
+	} else {
+		console.log(dataFeatures[0].center)
+	}
 })
 
+// DarkSky API Request
 request({ url: url, json: true }, ( error, response) => {
 	const data = response.body
 	// Error Handling
@@ -16,5 +25,4 @@ request({ url: url, json: true }, ( error, response) => {
 	} else {
 		console.log(`${data.currently.icon}. It is ${data.currently.temperature} out there. There is ${data.currently.precipProbability}% chance of rainfall`)
 	}
-
 })
