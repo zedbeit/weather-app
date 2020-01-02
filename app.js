13e85ee10d4ea0d184ac64858af1a1b38f3f9c89
deehaz <your.beitazaheed5@gmail.com>
2020-01-02 03:44:15 -0800

    add forecast fxn invocation

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/d97b2f7c517de91eca994ef49a5402cc/37.8267,-122.4233'
-// // DarkSky API Request
-// request({ url: url, json: true }, ( error, response) => {
-// 	// Error Handling
-// 	if(error) {
-// 		console.log('Unable to connect to the Weather service, please check your internet connection')
-// 	} else if(data.error) {
-// 		console.log(`Invalid request`)
-// 	} else {
-// 		const data = response.body
-// 		console.log(`${data.currently.icon}. It is ${data.currently.temperature} out there. There is ${data.currently.precipProbability}% chance of rainfall`)
-// 	}
-// })
 
+// geocode function Invocation
 geocode('Boston', (error, data) => {
 	console.log('Error', error)
 	console.log('Data', data)
+})
+
+// forecast function Invocation
+forecast('', 44.1545, (error, data) => {
+	if(error) console.log('Error: ', error)
+	else console.log('Data: ', data)
 })
