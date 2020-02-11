f8fac36f02ca34f6c274a0d4468ed926767e6739
zedbeit <devbeita@gmail.com>
2020-02-11 07:50:56 +0100

    deploy to heroku server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) =>{
     displayText1.textContent = 'Loading...'
     // displayText2.textContent = 'Loading...2'
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then( response => {
+    fetch(`/weather?address=${location}`).then( response => {
         response.json().then(data => {
             if(data.error) {
                 displayText1.textContent = data.error
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const members = require('./utils/member')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Paths
 const publicPathDir = path.join(__dirname, '../public')
@@ -109,6 +110,6 @@ app.get('*', (req, res) => {
 	res.send('My 404 page')
 })
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000')
+app.listen(port, () => {
+	console.log('Server is up on port at ' + port)
 })
